30d70867cf93f182f85319f53e2def8ef1520665
Basit ali <basitalyshah51214@gmail.com>
2025-11-24 00:01:05 -0800

    Refactor color definitions to use Color class and add MyText widget for enhanced text styling

--- lib/controllers/app_colors.dart
+++ lib/controllers/app_colors.dart
@@ -1,14 +1,16 @@
+import 'package:flutter/widgets.dart';
+
 class AppColors {
-  static const int primary = 0xFFFFFFFF;
-  static const int primaryVariant = 0xFF3700B3;
-  static const int secondary = 0xFF03DAC6;
-  static const int secondaryVariant = 0xFF018786;
-  static const int background = 0x000000;
-  static const int surface = 0xFFFFFFFF;
-  static const int error = 0xFFB00020;
-  static const int onPrimary = 0xFFFFFFFF;
-  static const int onSecondary = 0xFF000000;
-  static const int onBackground = 0xFF000000;
-  static const int onSurface = 0xFF000000;
-  static const int onError = 0xFFFFFFFF;
+  static const Color primary = Color(0xFFFFFFFF);
+  static const Color primaryVariant = Color(0xFF3700B3);
+  static const Color secondary = Color(0xFF03DAC6);
+  static const Color secondaryVariant = Color(0xFF018786);
+  static const Color background = Color(0xff000000);
+  static const Color surface = Color(0xFFFFFFFF);
+  static const Color error = Color(0xFFB00020);
+  static const Color onPrimary = Color(0xFFFFFFFF);
+  static const Color onSecondary = Color(0xFF000000);
+  static const Color onBackground = Color(0xFF000000);
+  static const Color onSurface = Color(0xFF000000);
+  static const Color onError = Color(0xFFFFFFFF);
 }
